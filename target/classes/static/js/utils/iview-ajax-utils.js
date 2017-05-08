d62f79b1c796492e5ec1e8142c789639118b0782
beamofsoul <beamofsoul@sina.com>
2017-05-08 08:58:33 +0800

    清除项目输出目录

--- target/classes/static/js/utils/iview-ajax-utils.js
+++
@@ -1,8 +0,0 @@
-$.iposty = function(url, data, successCallback, errorCallback) {
-		if(!errorCallback) errorCallback = defaultErrorCallback;
-		$.posty(url, data, successCallback, errorCallback);
-}
-
-function defaultErrorCallback(errorMessage){
-	vueContentObject.$Message.error(errorMessage);
-}
