5d7185fe88093efa0b7f8635de2f668108a2fd24
beamofsoul <beamofsoul@sina.com>
2017-05-16 11:31:33 +0800

    整合所有hotkeys utils方法

--- src/main/resources/static/js/utils/hotkeys-utils.js
+++ src/main/resources/static/js/utils/hotkeys-utils.js
@@ -31,3 +31,24 @@ function hotkey(keys,fn) {
 
 	document.addEventListener("keydown", killSpaceBar, false);
 })();
+
+$(document).ready(function(){
+	//伸出与收缩queryForm
+	hotkey('space+q', function() {getVueRefObject('defaultVueBindCollapseQueryFormData').value = String(parseInt(getVueRefObject('defaultVueBindCollapseQueryFormData').value) * -1)});
+	//弹出与关闭addForm
+	hotkey('space+a', function() {if(!vueContentObject.defaultVueBindModalAddData) vueContentObject.vueBindButtonHeadAddMethod(); else vueContentObject.defaultVueBindModalAddData = false});
+	//弹出addForm后，进行自动提交表单
+	hotkey('space+s', function() {if(vueContentObject.defaultVueBindModalAddData) vueContentObject.vueBindButtonHeadAddSubmitMethod()});
+	//弹出addForm后，进行表单重置
+	hotkey('space+r', function() {if(vueContentObject.defaultVueBindModalAddData) resetVueFormData('defaultVueBindFormAddData')});
+	//回归页面顶部
+	hotkey('space+b', function() {$('.ivu-back-top').click()});
+	//数据表格上一页
+	hotkey('space+n', function() {$('.ivu-page-prev').click()});
+	//数据表格下一页
+	hotkey('space+m', function() {$('.ivu-page-next').click()});
+	//数据表格第一页
+	hotkey('space+,', function() {vueContentObject.defaultVueBindPageCurrentData = 1;vueContentObject.vueBindPageOnChangeMethod(1)});
+	//数据表格最后一页
+	hotkey('space+.', function() {var pageFinal = Math.ceil(vueContentObject.defaultVueBindPageTotalData / vueContentObject.defaultVueBindPageSizeData);vueContentObject.defaultVueBindPageCurrentData = pageFinal;vueContentObject.vueBindPageOnChangeMethod(pageFinal)});
+});
