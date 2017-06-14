c011896450c64be8b82c05a44b16a2ed4a6d8ca6
beamofsoul <beamofsoul@sina.com>
2017-06-14 10:13:49 +0800

    删除无用注释

--- src/main/java/com/beamofsoul/bip/ApplicationEntry.java
+++ src/main/java/com/beamofsoul/bip/ApplicationEntry.java
@@ -19,23 +19,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ApplicationEntry extends SpringBootServletInitializer  {
 	
 	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder builder) {
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(ApplicationEntry.class);
 	}
 	
 	public static void main(String[] args) {
-		/**
-		 * 关闭Banner - 启动服务器后用符号拼接成的[Spring]图形
-		 * SpringApplication app = new SpringApplication(ApplicationEntry.class);
-		 * app.setBannerMode(Banner.Mode.OFF);
-		 * app.run(args); 
-		 * 
-		 * PS: 1.Banner.Mode.OFF:关闭
-		 *     2.Banner.Mode.CONSOLE:控制台输出，默认方式
-		 *     3.Banner.Mode.LOG:日志输出方式
-		 *     4.或在properties文件中设置spring.main.show-banner=false
-		 */
 		SpringApplication.run(ApplicationEntry.class, args);
 	}
 }
